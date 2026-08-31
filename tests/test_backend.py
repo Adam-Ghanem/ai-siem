c6cc0061da38796eda62654cc2f481e4a085d976
Adam Ghanem <adam.ghanem.it@gmail.com>
2026-08-31 18:00:05 +0100

    test: define event search API contract

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -29,6 +29,21 @@ class BackendApiTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertGreater(len(response.json()), 0)
 
+    def test_event_search_exposes_storage_native_filters_and_pagination(self):
+        sample = main.EVENTS[0]
+        response = self.client.get(
+            '/api/search/events',
+            params={'source': sample.source, 'limit': 1, 'offset': 0},
+            headers=AUTH,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+        self.assertEqual(response.json()[0]['source'], sample.source)
+        self.assertGreaterEqual(int(response.headers['X-Total-Count']), 1)
+        self.assertEqual(response.headers['X-Page-Limit'], '1')
+        self.assertEqual(response.headers['X-Page-Offset'], '0')
+
     def test_metrics_total_events_matches_loaded_events(self):
         events = self.client.get('/api/events', headers=AUTH).json()
         metrics_response = self.client.get('/api/metrics', headers=AUTH)
